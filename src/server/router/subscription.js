286c6d76b3b590900846054563789e6b0fcb2ad3
gtakat <gtakat@gmail.com>
2021-03-15 17:03:47 +0900

    tax設定をconfigに移動

--- src/server/router/subscription.js
+++ src/server/router/subscription.js
@@ -20,13 +20,15 @@ router.post('/create-subscription', async (req, res) => {
     error: false
   }
 
+  const default_tax_rates = require('/var/app/config.json')['default_tax_rates']
+
   try {
     // default_payment_methodも渡せるが、指定しない場合は
     // customer.invoice_settings.default_payment_methodが使用される
     const params = {
       customer: customer_id,
       items: [{ price: price_id }],
-      default_tax_rates: ['txr_1IBZAqIH8r0fTJ7pPAEnwMdM'],
+      default_tax_rates: default_tax_rates,
       expand: ['latest_invoice.payment_intent']
     }
 
